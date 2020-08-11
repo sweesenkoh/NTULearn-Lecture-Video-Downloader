2f922d8765761b44173730056f5531d73b96b1cf
WTT <wilsonteng89@gmail.com>
2020-08-11 14:32:02 +0800

    Update NTUContentDownloaderConsoleApp.py
    - Added Download All option

--- NTUContentDownloaderConsoleApp.py
+++ NTUContentDownloaderConsoleApp.py
@@ -87,17 +87,16 @@ def navigate_folder(href, folder_name, path_name, level=1):
 
 
 while True:
-    if (password is None) or (username is None):
-        username = input("Please input ntulearn username:   ")
-        password = getpass.getpass('Password:')
-        errorCheck = input("Are you sure the information are correct? press n to input again, press any other key to continue..")
-        if (errorCheck == "n"):
-            continue
+    # if (password is None) or (username is None):
+    #     username = input("Please input ntulearn username:   ")
+    #     password = getpass.getpass('Password:')
+    #     errorCheck = input("Are you sure the information are correct? press n to input again, press any other key to continue..")
+    #     if (errorCheck == "n"):
+    #         continue
 
     print("\n Loading... \n\n")
     login_url = "https://ntulearn.ntu.edu.sg/webapps/login/"
 
-    options.add_argument("user-data-dir=chrome-data") 
     driver.get(login_url)
     time.sleep(2)
     # driver.implicitly_wait(2)
@@ -130,32 +129,40 @@ while True:
         break
 
 while True:
-    titleLst = []
+    courseLst = []
 
     for div in mydivs:
         soup = BeautifulSoup(str(div),features="lxml")
         myList = soup.findAll("li")
 
         for lst in myList:
-            title = BeautifulSoup(str(lst),features="lxml").findAll("a")
-            title = (title[0].get_text())
-            titleLst.append([title])
+            course = BeautifulSoup(str(lst),features="lxml").findAll("a")
+            course = course[0].get_text()
+            courseLst.append(course)
 
     print(f"Welcome to NTU Lecture Content Downloader (CWD : '{CWD}')")
     print("\n   Please Select the Subject: ")
-    for (index,item) in enumerate(titleLst):
-        print(str(index + 1) + ") " + item[0])
+    for (index,item) in enumerate(courseLst):
+        print(str(index + 1) + ") " + item)
+    
+    download_all_int = index + 2
+    print("--------------------------------")
+    print(f"{download_all_int}) --- Download All")
 
     print("\n\n")
 
     while True:
         userChoice = (input("Please enter the course number: "))
         try:
+            all = False
             userChoiceInt = int(userChoice)
-            if (userChoiceInt <= 0 or userChoiceInt > len(titleLst)):
+            if (userChoiceInt <= 0 or userChoiceInt > len(courseLst)+1):
                 raise IndexError
-            userChoiceTitle = str(titleLst[userChoiceInt - 1][0])
-            print(userChoiceTitle)
+            if userChoiceInt == download_all_int:
+                userChoiceCourseLst = courseLst
+            else:
+                userChoiceCourseLst = [str(courseLst[userChoiceInt - 1])]
+            print(userChoiceCourseLst)
             break
         except ValueError:
             print("Please input only digits, not any other character: \n")
@@ -164,53 +171,56 @@ while True:
             print("Invalid Input, the choice is not listed, please try again: \n")
             continue
   
-    skip = False
-    
-    element = driver.find_element_by_link_text(userChoiceTitle)
-    driver.execute_script("arguments[0].click();", element)
-
-    try:
-        element = driver.find_element_by_partial_link_text("Content")
+    for course in userChoiceCourseLst:
+        skip = False
+        print(f"Curr course : {course}")
+        element = driver.find_element_by_link_text(course)
         driver.execute_script("arguments[0].click();", element)
-        time.sleep(1)
-        # driver.implicitly_wait(2)
-    except:
-        skip = True
-        print("[!INFO] No content to download.\n")
 
-    if (not skip):    
-        soup = BeautifulSoup(driver.page_source,features="lxml")
-        containerHTML = soup.findAll("ul", {"class": "contentList"})[0]
-        anonymous_elements = re.findall("anonymous_element_\d+",str(containerHTML))
-        print("\n")
-        print("      List of Content      ")
-        print("=============================")
-        ContentNames = BeautifulSoup(str(containerHTML),features="lxml").find("ul").findAll("li", recursive=False)
-        print("Parent Length : ", len(ContentNames))
-        ContentNamesList = []
-        for (index, contentName) in enumerate(ContentNames):
-            children = BeautifulSoup(str(contentName),features="lxml").findAll("a", href=True)
-            for child in children:
-                name = child.get_text()
-                print(f"{index + 1}) {name} ")
-                ContentNamesList.append(name)
-                url = check_url(child['href'])
-                if "/webapps/blackboard/content/listContent.jsp" in child['href']:
-                    # is folder
-                    navigate_folder(url, name, userChoiceTitle.replace(":", ""))
-                elif "/bbcswebdav/" in child['href']:
-                    # is file
-                    download_file(url, os.path.join(userChoiceTitle, str(name)).replace(":", ""))
+        try:
+            element = driver.find_element_by_partial_link_text("Content")
+            driver.execute_script("arguments[0].click();", element)
+            time.sleep(1)
+            # driver.implicitly_wait(2)
+        except:
+            skip = True
+            print("[!INFO] No content to download.\n")
 
-        print("--------------------------------")
-        print("[!INFO] Download finished\n")
+        if (not skip):    
+            soup = BeautifulSoup(driver.page_source,features="lxml")
+            containerHTML = soup.findAll("ul", {"class": "contentList"})[0]
+            anonymous_elements = re.findall("anonymous_element_\d+",str(containerHTML))
+            print("\n")
+            print("      List of Content      ")
+            print("=============================")
+            ContentNames = BeautifulSoup(str(containerHTML),features="lxml").find("ul").findAll("li", recursive=False)
+            print("Parent Length : ", len(ContentNames))
+            ContentNamesList = []
+            for (index, contentName) in enumerate(ContentNames):
+                children = BeautifulSoup(str(contentName),features="lxml").findAll("a", href=True)
+                for child in children:
+                    name = child.get_text()
+                    print(f"{index + 1}) {name} ")
+                    ContentNamesList.append(name)
+                    url = check_url(child['href'])
+                    if "/webapps/blackboard/content/listContent.jsp" in child['href']:
+                        # is folder
+                        navigate_folder(url, name, course.replace(":", ""))
+                    elif "/bbcswebdav/" in child['href']:
+                        # is file
+                        download_file(url, os.path.join(course, str(name)).replace(":", ""))
 
+            print("--------------------------------")
+            print(f"[!INFO] Download finished for [{course}]\n")
+
+            driver.switch_to.window(driver.window_handles[0])
+            driver.get(mainCourseLink)
     # print(ContentNamesList)
 
-    userContinueChoice = input("Do u want to download more files? \nPress y to continue, press any other key to quit\n")
-    if (userContinueChoice != "y" and userContinueChoice != "Y"):
-        print("Bye")
-        break
+    # userContinueChoice = input("Do u want to download more files? \nPress y to continue, press any other key to quit\n")
+    # if (userContinueChoice != "y" and userContinueChoice != "Y"):
+    #     print("Bye")
+    #     break
 
     driver.switch_to.window(driver.window_handles[0])
     driver.get(mainCourseLink)
